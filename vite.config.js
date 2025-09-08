dd712eecdf00991d0d935cc4bd8fa40a4316fbb7
Dennisonya <dennisonya3@gmail.com>
2025-09-08 23:36:29 +0300

    config fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
+  server: {
     host: 'vibedeck.local', // or '0.0.0.0'
-    port: 5174
+    port: 5174,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // CHANGE if your backend runs on another port
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
 
